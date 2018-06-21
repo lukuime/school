23128d6cbddb91e57dba10b006f13de20af62ac0
lucky <lukuime@163.com>
2018-06-21 08:48:44 +0800

    第一次提交

--- Application/Common/Conf/test.php
+++ Application/Common/Conf/test.php
@@ -11,9 +11,9 @@ return array(
     // 数据库配置信息
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'schoolcms_2.3', // 数据库名
+    'DB_NAME'   => 'school', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root', // 密码
+    'DB_PWD'    => '', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PARAMS' =>  array(), // 数据库连接参数
     'DB_PREFIX' => 'sc_', // 数据库表前缀 
